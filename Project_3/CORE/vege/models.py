c6b310b9b4ebcfb98beeec7b7f545d8c1776f338
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2023-12-18 17:31:43 -0800

    Delete Project_3/CORE directory

--- Project_3/CORE/vege/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Reciepe(models.Model):
-    reciepe_name = models.CharField(max_length = 100)
-    reciepe_descriptin = models.TextField()
-    reciepe_image = models.ImageField(upload_to= "reciepe")
-
-    
